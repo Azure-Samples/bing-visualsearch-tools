c87e1796b622ffb7209c60dab018eb624f2e5799
Kush Singh <t-kusing@microsoft.com>
2018-08-01 12:35:42 -0700

    Removed unnecessary class

--- src/VSPingShared/Utils/Helpers.cs
+++ src/VSPingShared/Utils/Helpers.cs
@@ -77,55 +77,6 @@ namespace VSPing.Utils
         public Newtonsoft.Json.Linq.JToken JToken { get; set; }
     }
 
-    // Copied from Aether Client Code
-    public static class ClipboardUtils
-    {
-        // Copied from Aether Client Code
-        public static string CreateHtmlClipboardFormat(string sHtml)
-        {
-            // See MSDN for "http://msdn.microsoft.com/en-us/library/ms649015(VS.85).aspx" for
-            // the details on the HTML Clipboard Format.
-
-            // Magic header with 10 digits of space reserved for the header fields
-            const string cClipboardMagic = @"Version:1.0
-StartHTML:*********1
-EndHTML:*********2
-StartFragment:*********3
-EndFragment:*********4
-StartSelection:*********3
-EndSelection:*********4
-";
-            //string sPre = "<html><head><title></title></head><body><!--StartFragment-->";
-            const string cPre = "<html><body><!--StartFragment-->";
-            const string cPost = "<!--EndFragment--></body></html>";
-
-            var sbOut = new StringBuilder();
-
-            // Concatenate the text, caching the offsets as we go.
-            sbOut.Append(cClipboardMagic);
-            int ibHtmlStart = sbOut.Length;
-            sbOut.Append(cPre);
-            int ibFragmentStart = sbOut.Length;
-            sbOut.Append(sHtml);
-            int ibFragmentEnd = sbOut.Length;
-            sbOut.Append(cPost);
-            int ibHtmlEnd = sbOut.Length;
-
-            // Replace the placeholders with the appropriate offset.
-            sbOut.Replace("*********1", ibHtmlStart.ToString("d10"));
-            sbOut.Replace("*********2", ibHtmlEnd.ToString("d10"));
-            sbOut.Replace("*********3", ibFragmentStart.ToString("d10"));
-            sbOut.Replace("*********4", ibFragmentEnd.ToString("d10"));
-
-            return sbOut.ToString();
-        }
-
-        public static string CreateHtmlLink(string sLink)
-        {
-            return string.Format("<a href=\"{0}\">{0}</a>", sLink);
-        }
-    }
-
     public static class StringUtils
     {
         public static string ToXmlEscapedString(this string inString)
